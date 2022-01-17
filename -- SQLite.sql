8540f470b68e6ea908e2f5e8471a27dd17d97c56
GitProfAn <serzhe@mail.ru>
2022-01-17 15:23:45 +0300

    up to page 115

--- -- SQLite.sql
+++ -- SQLite.sql
@@ -226,9 +226,9 @@
 -- WHERE SOUNDEX(cust_contact) = SOUNDEX('Michael Green');
 
 -- Функции для работы с датой и временем
-SELECT order_num
-FROM Orders
-WHERE strftime('%Y', order_date) = 2012;
+-- SELECT order_num
+-- FROM Orders
+-- WHERE strftime('%Y', order_date) = 2012;
 
 -- стр. 93
 -- Урок 9
@@ -281,3 +281,41 @@ WHERE strftime('%Y', order_date) = 2012;
 -- стр. 105
 -- Урок 10
 -- Группировка данных
+
+-- Создание групп
+-- SELECT vend_id, COUNT(*) AS num_prods
+-- FROM Products
+-- GROUP BY vend_id;
+
+-- Фильтрация по группам
+-- SELECT cust_id, COUNT(*) AS orders
+-- FROM Orders
+-- GROUP BY cust_id
+-- HAVING COUNT(*) >= 2;
+
+-- SELECT vend_id, COUNT(*) AS num_prods
+-- FROM Products
+-- WHERE prod_price >= 4
+-- GROUP BY vend_id
+-- HAVING COUNT(*) >= 2;
+
+-- SELECT vend_id, COUNT(*) AS num_prods
+-- FROM Products
+-- GROUP BY vend_id
+-- HAVING COUNT(*) >= 2;
+
+-- Группировка и сортировка
+-- SELECT order_num, COUNT(*) AS items
+-- FROM OrderItems
+-- GROUP BY order_num
+-- HAVING COUNT(*) >= 3;
+
+-- SELECT order_num, COUNT(*) AS items
+-- FROM OrderItems
+-- GROUP BY order_num
+-- HAVING COUNT(*) >= 3
+-- ORDER BY items, order_num;
+
+-- стр. 115
+-- Урок 11
+-- Подзапросы
